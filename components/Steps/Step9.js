3175433caaa99d5761e632995f0f21d7026826d0
kevin <kevin088.rejoice@gmail.com>
2023-03-22 16:58:06 +0530

    Changed live link

--- components/Steps/Step9.js
+++ components/Steps/Step9.js
@@ -162,7 +162,7 @@ export default function Step9() {
       interesse_finanzierung: userData.step4,
       dachform: userData.step3,
       art_heizung: userData.step5,
-      leadherkunft: "https://josefashome.netlify.app",
+      leadherkunft: "https://josefas.home.solar-pvcheck.de",
       Bemerkungen: calculatedDa,
       project: "josefashome"
     };
